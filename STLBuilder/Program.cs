80537d74f35b01af30ce0bce1b6dce90b419e5e4
Sam Wronski <runewake2@outlook.com>
2017-05-30 22:15:52 -0500

    Add project files.

---
+++ STLBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace STLBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
